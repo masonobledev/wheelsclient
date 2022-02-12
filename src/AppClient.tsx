974018233c8b19e9819ac260cfc593ca7ab530b8
Patrick Cook <masonobledev@gmail.com>
2022-02-12 16:10:28 -0500

    first client push

---
+++ src/AppClient.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const AppClient = () => {
+  return (
+    <div className="App">
+        <div className="verticalCenter">
+        </div>  
+    </div>
+  );
+}
+
+export default AppClient;
---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import AppClient from './AppClient';
+
+export const Router: React.FunctionComponent = () => {
+    return(
+        <BrowserRouter>
+        <div>
+            <Route exact={true} path="/" component={AppClient} />
+        </div>
+        </BrowserRouter>
+    );
+};
